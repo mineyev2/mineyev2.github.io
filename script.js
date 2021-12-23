ea97a844fb095192b4c51dccb0b46ccae24deeea
FIRST_NAME LAST_NAME <mineyev2@illinois.edu>
2021-12-23 17:40:23 -0600

    gif abackground works!

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
 var h;
 var w;
 
+// var canvas;
+
+var gif_loadImg, gif_createImg;
+
 var frame = 0;
 console.log("Change");
+
+function preload() {
+	gif_createImg = createImg("assets/background1.gif");
+}
 
 //initialize canvas
 function setup() {
@@ -11,45 +19,22 @@ function setup() {
 	w = window.innerWidth;
   	canvas = createCanvas(w, h);
   	canvas.parent("canv")
+	background(0);
 }
 
-//updating function
 function draw() {
-	console.log(w, h);
-	background(255, 255, 255);
-
-	bottomColor = [255, 184, 219]
-	remColor = [181 - 255, 204 - 184, 255 - 219]
-
-	for (var i = 0; i < h; i++) {
-		midColor = [bottomColor[0] + remColor[0]*i/h, bottomColor[1] + remColor[1]*i/h, bottomColor[2] + remColor[2]*i/h]
-		stroke(color(midColor[0], midColor[1], midColor[2]))
-		strokeWeight(1);
-		line(0, i, w, i)
-	}
-
-	stroke(color(255, 255, 255));
-	//strokeWeight(4);
-
-	line(0, 2*(h/1.2)/3, w, 2*(h/1.2)/3);
-
-	for (var i = 0; i < w/2; i += 50) {
-		line(w/2 - i, 2*(h/1.2)/3, w/2 - 2*i, h)
-	} 
-
-	for (var i = 0; i < w/2; i += 50) {
-		line(w/2 + i, 2*(h/1.2)/3, w/2 + 2*i, h)
-	} 
-
-	for (var i = 2*(h/1.2)/3 + (frame % 50); i < h; i += 50) {
-		line(0, i, w, i);
-	}
-
-	frame++; 
+	// loads only first frame
+	// image(gif_loadImg, 50, 50);
+	
+	// updates animation frames by using an html
+	// img element, positioning it over top of
+	// the canvas.
+	gif_createImg.position(0, 0);
 }
 
 window.onresize = function() {
 	h = window.innerHeight * 1.2;
 	w = window.innerWidth;
 	canvas.resize(w, h);
+	// gif_createImg.resize(w, h);
 }
